01fcdc4f3f77518a578ef631c002b9c0149516c3
Angela Lynn Smith <angiesmith1226@gmail.com>
2024-01-27 19:04:15 -0500

    deletes js and pevent horiz scroll

--- index.js
+++
@@ -1,7 +0,0 @@
-function printElement(e) {
-	let cloned = e.cloneNode(true);
-	document.body.appendChild(cloned);
-	cloned.classList.add("printable");
-	window.print();
-	document.body.removeChild(cloned);
-}
